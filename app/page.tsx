a7690fb73082d1dadea54cfc68928439672c4350
Debojyoti Mishra <105543436+DebojyotiMishra@users.noreply.github.com>
2024-11-23 22:54:36 +0100

    linter test

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
     <div >
       <p className="text-red-900 font-bold text-7xl">Hello Atlas</p>
+      <Button>Click me!</Button>
     </div>
   );
 }
