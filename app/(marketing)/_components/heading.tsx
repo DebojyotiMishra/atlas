7ab6c3d7a09700b153ef50a22e839ef7f868d56d
Debojyoti Mishra <105543436+DebojyotiMishra@users.noreply.github.com>
2024-11-25 00:19:49 +0100

    nav

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -13,7 +13,7 @@ export const Heading = () => {
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
         Your Ideas, Documents & Plans. Unified. Welcome to{" "}
-        <span className="underline">Atlas</span>.
+        <span className="underline">Atlas</span>
       </h1>
       <h3 className="text-base sm:text-xl md:text-2xl font-normal">
         Atlas is the all-in-one workspace for your notes, documents, and plans.
--- components/spinner.tsx
+++ components/spinner.tsx
@@ -1,32 +1,26 @@
-import { Loader } from 'lucide-react';
+import { Loader } from "lucide-react";
 
-import { cva, type VariantProps } from 'class-variance-authority';
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from '@/lib/utils';
+import { cn } from "@/lib/utils";
 
-const spinnerVariants = cva(
-    "text-muted-foreground animate-spin",
-    {
-        variants: {
-            size: {
-                default: "w-4 h-4",
-                sm: "w-3 h-3",
-                lg: "w-6 h-6",
-                icon: "w-8 h-8",
-            },
-        },
-        defaultVariants: {
-            size: "default",
-        },
-    }
-);
+const spinnerVariants = cva("text-muted-foreground animate-spin", {
+  variants: {
+    size: {
+      default: "w-4 h-4",
+      sm: "w-3 h-3",
+      lg: "w-6 h-6",
+      icon: "w-8 h-8",
+    },
+  },
+  defaultVariants: {
+    size: "default",
+  },
+});
 
+/* trunk-ignore(eslint/@typescript-eslint/no-empty-object-type) */
 interface SpinnerProps extends VariantProps<typeof spinnerVariants> {}
 
 export const Spinner = ({ size }: SpinnerProps) => {
-    return (
-        <Loader
-            className={cn(spinnerVariants({ size }))}
-        />
-    );
-}
+  return <Loader className={cn(spinnerVariants({ size }))} />;
+};
