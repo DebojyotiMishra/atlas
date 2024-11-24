c73e15e9534f65018d91126bbf0bce02949262aa
Debojyoti Mishra <105543436+DebojyotiMishra@users.noreply.github.com>
2024-11-24 01:05:30 +0100

    sync

---
+++ app/(marketing)/_components/heading.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+
+export const Heading = () => {
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+        Your Ideas, Documents & Plans. Unified. Welcome to <span className="underline">Atlas</span>.
+      </h1>
+      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+        Atlas is the all-in-one workspace for your notes, documents, and plans. It&apos;s like a second brain, but better.
+      </h3>
+      <Button>Enter Atlas<ArrowRight className="h-4 w-4" /></Button>
+    </div>
+  );
+};
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,10 +1,16 @@
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
+import { Heading } from "./_components/heading";
+import { Heroes } from "./_components/heroes";
 
-export default function Home() {
+const MarketingPage = () => {
   return (
-    <div>
-      <p className="text-red-900 font-bold text-7xl">Hello Atlas</p>
-      <Button>Click me!</Button>
+    <div className="min-h-full flex flex-col">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+        <Heading />
+        <Heroes></Heroes>
+      </div>
     </div>
   );
 }
+
+export default MarketingPage;
