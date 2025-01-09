64ab8711edd0d283aebac7b4abd460f67bc40f9b
Debojyoti Mishra <105543436+DebojyotiMishra@users.noreply.github.com>
2025-01-09 22:05:47 +0100

    set up adding Cover Image funtionality

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,79 +1,39 @@
-// "use client";
+"use client";
 
-// import dynamic from "next/dynamic";
-// import { useMemo } from "react";
+import { use } from "react";
+import Toolbar from "@/components/toolbar";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
 
-// import { Cover } from "@/components/cover";
-// import { Toolbar } from "@/components/toolbar";
-// import { Skeleton } from "@/components/ui/skeleton";
+interface DocumentIdPageProps {
+  params: {
+    documentId: Id<"documents">;
+  };
+}
 
-// import { api } from "@/convex/_generated/api";
-// import { Id } from "@/convex/_generated/dataModel";
-// import { useMutation, useQuery } from "convex/react";
+const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
+    const unwrappedParams = use(params);
+    const document = useQuery(api.documents.getById, {
+        documentId: unwrappedParams.documentId,
+    });
 
-// interface DocumentIdPageProps {
-//   params: {
-//     documentId: Id<"documents">;
-//   };
-// }
+    if (document === undefined) {
+        return <div>Loading...</div>;
+    }
 
-// const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
-//   const Editor = useMemo(
-//     () => dynamic(() => import("@/components/editor"), { ssr: false }),
-//     [],
-//   );
+    if (document === null) {
+        return <div>Not found</div>;
+    }
 
-//   const document = useQuery(api.documents.getById, {
-//     documentId: params.documentId,
-//   });
-
-//   const update = useMutation(api.documents.update);
-
-//   const onChange = (content: string) => {
-//     update({
-//       id: params.documentId,
-//       content,
-//     });
-//   };
-
-//   if (document === undefined) {
-//     return (
-//       <div>
-//         <Cover.Skeleton />
-//         <div className="mx-auto mt-10 md:max-w-3xl lg:max-w-4xl">
-//           <div className="space-y-4 pl-8 pt-4">
-//             <Skeleton className="h-14 w-1/2" />
-//             <Skeleton className="h-4 w-4/5" />
-//             <Skeleton className="h-4 w-2/5" />
-//             <Skeleton className="h-4 w-3/5" />
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   }
-
-//   if (document === null) {
-//     return <div>Not found</div>;
-//   }
-
-//   return (
-//     <div className="pb-40">
-//       <Cover url={document.coverImage} />
-//       <div className="mx-auto md:max-w-3xl lg:max-w-4xl">
-//         <Toolbar initialData={document} />
-//         <Editor onChange={onChange} initialContent={document.content} />
-//       </div>
-//     </div>
-//   );
-// };
-// export default DocumentIdPage;
-
-const DocumentIdPage = () => {
     return (
-        <div>
-            Document ID Page
+        <div className="pb-40">
+            <div className="h-[35vh]"></div>
+            <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+                <Toolbar initialData={document}></Toolbar>
+            </div>
         </div>
-     );
+    );
 }
 
 export default DocumentIdPage;
---
+++ components/modals/cover-image-modal.tsx
@@ -0,0 +1,61 @@
+"use client";
+import useCoverImage from "@/hooks/useCoverImage";
+import React, { useState } from "react";
+import { Dialog, DialogContent, DialogHeader } from "@/components/ui/dialog";
+// import { useEdgeStore } from "@/lib/edgestore";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useParams } from "next/navigation";
+import { Id } from "@/convex/_generated/dataModel";
+// import { SingleImageDropzone } from "@/components/ui/single-image-dropzone";
+
+const CoverImageModal = () => {
+  const params = useParams();
+  const update = useMutation(api.documents.update);
+  const [file, setFile] = useState<File>();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  // const { edgestore } = useEdgeStore();
+  const coverImage = useCoverImage();
+
+  const onClose = () => {
+    setFile(undefined);
+    setIsSubmitting(false);
+    coverImage.onClose();
+  };
+
+  const onChange = async (file?: File) => {
+    if (file) {
+      setIsSubmitting(true);
+      setFile(file);
+
+      const res = await edgestore.publicFiles.upload({
+        file,
+        options: {
+          replaceTargetUrl: coverImage.url,
+        },
+      });
+
+      await update({
+        id: params.documentId as Id<"documents">,
+        coverImage: res.url,
+      });
+
+      onClose();
+    }
+  };
+
+  return (
+    <Dialog open={coverImage.isOpen} onOpenChange={coverImage.onClose}>
+      <DialogContent>
+        <DialogHeader>
+          <h2 className="text-center text-lg font-semibold">Cover Image</h2>
+        </DialogHeader>
+        <div>
+            TODO: UPLOAD IMAGE HERE
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default CoverImageModal;
